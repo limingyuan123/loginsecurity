945ca4536b9428a2ac6034951dcbb072d52b3ba1
limingyuan123 <1484156313@qq.com>
2019-10-14 19:44:20 +0800

    探索接收后端登陆数据

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-import HelloWorld from '@/components/HelloWorld'
 import Home from "../components/Home";
 
 Vue.use(Router)
